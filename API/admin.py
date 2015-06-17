e4a84e3ac9e7f0135f2855d12f94dc5ec104a8e6
KorolevskyMax <korolevsky.max@gmail.com>
2015-06-17 04:17:27 +0300

    weekly report

--- API/admin.py
+++ API/admin.py
@@ -1,12 +1,8 @@
 from django.contrib import admin
-from .models import Project, ProjectTeam
+from .models import Project, Worklog
 
 # Register your models here.
-class ProjectTeamLine(admin.StackedInline):
-    model = ProjectTeam
-    extra = 1
-
 class ProjectAdmin(admin.ModelAdmin):
-    inlines = [ProjectTeamLine]
     list_filter = ['title']
 admin.site.register(Project, ProjectAdmin)
+admin.site.register(Worklog)
