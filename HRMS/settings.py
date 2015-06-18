4f0aa829850bfa0187bc41d65dbad281a25286a6
KorolevskyMax <korolevsky.max@gmail.com>
2015-06-18 16:21:49 +0300

    fixed static

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -115,10 +115,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_files')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
     ('', os.path.join(BASE_DIR, 'static')),
-    ('', os.path.join(BASE_DIR, 'static_files')),
 )
 
 REST_FRAMEWORK = {
